7370bb15b0f69f93769a6820d728931583892307
Yair Yarom <irush@cs.huji.ac.il>
2017-01-11 18:28:54 +0200

    limit_interactive: read partition from configuration file

--- job_submit_limit_interactive.c
+++ job_submit_limit_interactive.c
@@ -1,5 +1,8 @@
 
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <slurm/slurm.h>
 #include <slurm/slurm_errno.h>
@@ -14,11 +17,42 @@ const char plugin_type[]="job_submit/limit_interactive";
 const uint32_t plugin_version = SLURM_VERSION_NUMBER;
 //const uint32_t min_plug_version = 100;
 
+static s_p_options_t limit_interactive_options[] = {
+	{"Partition", S_P_STRING},
+	{NULL}
+};
+
+char* limit_partition = NULL;
+
 extern int init (void) {
+    char *conf_file = NULL;
+    struct stat config_stat;
+    s_p_hashtbl_t *tbl = NULL;
+
+    // read conf file
+    conf_file = get_extra_conf_path("limit_interactive.conf");
+    if (stat(conf_file, &config_stat) < 0)
+        fatal("Can't stat conf file %s: %m", conf_file);
+
+    tbl = s_p_hashtbl_create(limit_interactive_options);
+
+    if (s_p_parse_file(tbl, NULL, conf_file, false) == SLURM_ERROR)
+        fatal("Can't parse limit_interactive.conf %s: %m", conf_file);
+
+    s_p_get_string(&limit_partition, "Partition", tbl);
+
+    s_p_hashtbl_destroy(tbl);
+    xfree(conf_file);
+
+    // validate
+    // FIXME, validate limit_interactive is a/are real partition(s)
+
     return SLURM_SUCCESS;
 }
 
 extern int fini (void) {
+    xfree(limit_partition);
+    limit_partition = NULL;
     return SLURM_SUCCESS;
 }
 
@@ -45,10 +79,12 @@ extern int job_submit(struct job_descriptor *job_desc, uint32_t submit_uid, char
         char* licstr = xstrdup("interactive:1");
         licstr[12] = ('0' + nlic);
 
-        if (job_desc->partition != NULL) {
-            xfree(job_desc->partition);
+        if (limit_partition) {
+            if (job_desc->partition != NULL) {
+                xfree(job_desc->partition);
+            }
+            job_desc->partition = xstrdup(limit_partition);
         }
-        job_desc->partition = xstrdup("short");
 
         char* tmp_str;
         if (job_desc->licenses == NULL) {
@@ -104,6 +140,9 @@ extern int job_modify(struct job_descriptor *job_desc, struct job_record *job_pt
         debug("limit_interactive: job_modify: not interactive, ignoring");
         return SLURM_SUCCESS;
     }
+
+    // for now, can't change anything on interactive jobs..
+    return ESLURM_NOT_SUPPORTED;
 
     // for now, can't change max_nodes
     if (job_desc->max_nodes != NO_VAL) {
